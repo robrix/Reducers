0a54ec422b441b15ef7fb172a64b30cea95e5e2e
Rob Rix <rob.rix@github.com>
2014-03-22 14:32:36 -0400

    Add predicates.

---
+++ Reducers/REDFilter.h
@@ -0,0 +1,44 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import <Reducers/REDReducible.h>
+
+#pragma mark Filter
+
+/// A block which specifies a predicate over an object.
+///
+/// \param subject The object to be predicated upon.
+///
+/// \return \c true if the object passes, \c false if it fails.
+typedef bool (^REDPredicateBlock)(id subject);
+
+
+#pragma mark Predicates
+
+/// A predicate which returns \c true.
+extern REDPredicateBlock const REDTruePredicateBlock;
+
+/// A predicate which returns \c false.
+extern REDPredicateBlock const REDFalsePredicateBlock;
+
+
+/// Returns a predicate which accepts objects equal to \c object.
+///
+/// \param object The object to compare against. May be nil.
+///
+/// \return \c true iff the subject and object are equal as determined by \c -isEqual:.
+REDPredicateBlock REDEqualityPredicate(id object);
+
+/// Returns a predicate which accepts objects identical to \c object.
+///
+/// \param object The object to compare against. May be nil.
+///
+/// \return \c true iff the subject and object are identical as determined by pointer equality.
+REDPredicateBlock REDIdentityPredicate(id object);
+
+
+/// Returns a predicate which inverts the sense of \c predicate.
+///
+/// \param predicate The predicate to apply and invert the sense of. Must not be nil.
+///
+/// \return \c true iff \c predicate returns \c false.
+REDPredicateBlock REDNotPredicate(REDPredicateBlock predicate);
