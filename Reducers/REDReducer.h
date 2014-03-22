559574e22a46099cf51c52029594720a575b61d1
Rob Rix <rob.rix@github.com>
2014-03-22 12:39:52 -0400

    Implements basic reducibles.

--- Reducers/REDReducer.h
+++ Reducers/REDReducer.h
@@ -2,5 +2,36 @@
 
 #import <Foundation/Foundation.h>
 
-@interface REDReducer : NSObject
+/// A binary block defining some piecewise reduction of a collection.
+///
+/// \param into The result of the previous step of the reduction.
+/// \param each The object being reduced at this step of the reduction.
+///
+/// \return The result of this step of the reduction, which will become the value of \c into passed to the next step of the reduction.
+typedef id(^REDReducingBlock)(id into, id each);
+
+
+/// A piecewise-reducible collection.
+@protocol REDReducible <NSObject>
+
+/// Produce a piecewise reduction of the receiver.
+///
+/// \param initial The initial result of the reduction. This should be some identity element of the operation, i.e. 0 for addition, 1 for multiplication, or an empty collection or string for appending.
+/// \param block The block used to reduce the receiver.
+///
+/// \return The result of applying the block to each eleemnt of the receiver, with \c initial used as the first result, and the result of the each successive step applied as the \c into parameter of the next step.
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block;
+
+@end
+
+
+#pragma mark Categories
+
+/// \c NSArray conforms to \c REDReducible
+@interface NSArray (REDReducible) <REDReducible>
+@end
+
+
+/// \c NSSet conforms to \c REDReducible
+@interface NSSet (REDReducible) <REDReducible>
 @end
--- Reducers/REDReducer.m
+++ Reducers/REDReducer.m
@@ -2,8 +2,37 @@
 
 #import "REDReducer.h"
 
-@implementation REDReducer
+#pragma mark Categories
+
+static inline id REDStrictReduce(id<NSFastEnumeration> collection, id initial, REDReducingBlock block) {
+	for (id each in collection) {
+		initial = block(initial, each);
+	}
+	return initial;
+}
+
+l3_addTestSubjectTypeWithFunction(REDStrictReduce)
+l3_test(&REDStrictReduce) {
+	NSArray *collection = @[ @"a", @"b", @"c" ];
+	id initial;
+	id (^lastObject)(id, id) = ^(id into, id each) { return each; };
+	l3_expect(REDStrictReduce(collection, initial, lastObject)).to.equal(collection.lastObject);
+}
 
 
+@implementation NSArray (REDReducible)
+
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
+	return REDStrictReduce(self, initial, block);
+}
+
+@end
+
+
+@implementation NSSet (REDReducible)
+
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
+	return REDStrictReduce(self, initial, block);
+}
 
 @end
