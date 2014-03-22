18cceb63cbf52db684f11919c6cf5b262f922d67
Rob Rix <rob.rix@github.com>
2014-03-22 12:05:32 -0400

    Initial Commit

---
+++ Reducers/Reducers.h
@@ -0,0 +1,13 @@
+//
+//  Reducers.h
+//  Reducers
+//
+//  Created by Rob Rix on 2014-03-22.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Reducers : NSObject
+
+@end
---
+++ ReducersTests/ReducersTests.m
@@ -0,0 +1,34 @@
+//
+//  ReducersTests.m
+//  ReducersTests
+//
+//  Created by Rob Rix on 2014-03-22.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReducersTests : XCTestCase
+
+@end
+
+@implementation ReducersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
