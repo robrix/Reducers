57ab23ae33ef26b27aa5cdd8716961a866be3609
Rob Rix <rob.rix@github.com>
2014-04-01 16:15:14 -0400

    Add an unimplemented REDJoin function & files.

---
+++ Reducers/REDJoin.h
@@ -0,0 +1,5 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import <Reducers/REDReducible.h>
+
+id<REDReducible> REDJoin(id<REDReducible> from, id separator);
---
+++ Reducers/REDJoin.m
@@ -0,0 +1,18 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "REDJoin.h"
+
+id<REDReducible> REDJoin(id<REDReducible> from, id separator) {
+	return nil;
+}
+
+l3_addTestSubjectTypeWithFunction(REDJoin)
+l3_test(&REDJoin) {
+	NSString *(^append)(NSString *, id) = ^(NSString *into, id each) { return [into stringByAppendingString:[each description]]; };
+	id<REDReducible> reducible = @[ @0, @1, @2, @3 ];
+	id separator = @".";
+	
+	id into = @"";
+	id joined = @"0.1.2.3";
+	l3_expect([REDJoin(reducible, separator) red_reduce:into usingBlock:append]).to.equal(joined);
+}
