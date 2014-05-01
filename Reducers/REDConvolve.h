c2c9879cbe1ec1f8f03cffae8b8a933b77972ae2
Rob Rix <rob.rix@github.com>
2014-05-01 15:00:58 -0400

    Stub in convolution.
    This doesn’t actually implement it yet.

--- Reducers/REDConvolve.h
+++ Reducers/REDConvolve.h
@@ -1,1 +1,26 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import <Reducers/REDReducible.h>
+
+#pragma mark Convolve
+
+/// A block for the piecewise mapping of multiple collections (convolution).
+///
+/// Although this is not declared with any parameters, it will actually receive one per reducible passed to \c REDConvolve.
+///
+/// \return  The object to be produced at each step of the convolution.
+typedef id(^REDConvolutionBlock)();
+
+
+/// A reducer which applies a convolution to each element of each of a collection of reducibles.
+///
+/// \param reducibles   A reducible of the reducibles to convolve.
+/// \param convolution  A block to be applied to each element of the reducibles in turn.
+/// \return             A reducible which, when reduced, maps the elements of the reducibles produced by \c reducibles with \c convolution.
+id<REDReducible> REDConvolve(id<REDReducible> reducibles, REDConvolutionBlock convolution);
+
+
+#pragma mark Array
+
+/// A convolution block which returns an array of its parameters.
+extern REDConvolutionBlock const REDArrayConvolutionBlock;
--- Reducers/REDConvolve.m
+++ Reducers/REDConvolve.m
@@ -1,1 +1,73 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "REDConvolve.h"
+#import "REDReducer.h"
+
+#pragma mark Convolve
+
+@interface REDConvolver : NSObject <REDReducible>
+
++(instancetype)convolverWithReducibles:(id<REDReducible>)reducibles convolution:(REDConvolutionBlock)convolution;
+
+@end
+
+id<REDReducible> REDConvolve(id<REDReducible> reducibles, REDConvolutionBlock convolution) {
+	return [REDConvolver convolverWithReducibles:reducibles convolution:convolution];
+}
+
+
+#pragma mark Array
+
+
+
+#pragma mark Convolution
+
+@implementation REDConvolver {
+	id<REDReducible> _reducibles;
+	REDConvolutionBlock _convolution;
+}
+
++(instancetype)convolverWithReducibles:(id<REDReducible>)reducibles convolution:(REDConvolutionBlock)convolution {
+	return [[self alloc] initWithReducibles:reducibles convolution:convolution];
+}
+
+-(instancetype)initWithReducibles:(id<REDReducible>)reducibles convolution:(REDConvolutionBlock)convolution {
+	if ((self = [super init])) {
+		_reducibles = reducibles;
+		_convolution = [convolution copy];
+	}
+	return self;
+}
+
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
+	// a = [1, 2, 3]
+	// b = [4, 5, 6]
+	// c = [7, 8, 9]
+	// all = [a, b, c]
+	//
+	// call(a1, b4, c7)
+	// call(a2, b5, c8)
+	// call(a3, b6, c9)
+	//
+	// a
+	// 1
+	//   \
+	//     b
+	//     4
+	//       \
+	//         c
+	//         7
+	//   <----
+	// a
+	// 2
+	//   \
+	//     etc
+	//
+	return [_reducibles red_reduce:_reducibles usingBlock:^(id<REDReducible> reducibles, id<REDReducible> reducible) {
+		return [reducible red_reduce:initial usingBlock:^(id into, id each) {
+			return into;
+		}];
+	}];
+}
+
+@end
