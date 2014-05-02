94599bb4191ef9bc370e8c072f3b7cd03fa643be
Rob Rix <rob.rix@github.com>
2014-05-02 17:16:25 -0400

    Doomed attempt at fauxroutines.

--- Reducers/REDConvolve.m
+++ Reducers/REDConvolve.m
@@ -57,17 +57,72 @@ id<REDReducible> REDConvolve(id<REDReducible> reducibles, REDConvolutionBlock co
 	//       \
 	//         c
 	//         7
-	//   <----
+	//  <-call-
 	// a
 	// 2
 	//   \
 	//     etc
 	//
-	return [_reducibles red_reduce:_reducibles usingBlock:^(id<REDReducible> reducibles, id<REDReducible> reducible) {
-		return [reducible red_reduce:initial usingBlock:^(id into, id each) {
-			return into;
-		}];
+	
+	NSMutableArray *queue = [NSMutableArray new];
+	NSMutableArray *arguments = [NSMutableArray new];
+	
+	typedef void (^REDQueueBlock)(void);
+	
+	REDQueueBlock (^dequeue)(void) = ^{
+		REDQueueBlock block = [queue firstObject];
+		if (block) [queue removeObjectAtIndex:0];
+		return block;
+	};
+	
+	void(^run)(void) = ^{
+		REDQueueBlock block;
+		while ((block = dequeue())) {
+			block();
+		}
+	};
+	
+	void(^enqueue)(REDQueueBlock) = ^(REDQueueBlock block) {
+		[queue addObject:block];
+	};
+	
+	// enqueue each column + the row col
+	
+	void (^callRow)(void) = ^{
+		NSLog(@"arguments: %@", arguments);
+		[arguments removeAllObjects];
+		run();
+	};
+	
+	REDQueueBlock flow = [_reducibles red_reduce:callRow usingBlock:^(REDQueueBlock block, id<REDReducible> column) {
+		return (id)^{
+			[column red_reduce:nil usingBlock:^(id _, id cell) {
+				run();
+				enqueue(^{
+					[arguments addObject:cell];
+				});
+				return cell;
+			}];
+			
+			enqueue(block);
+		};
 	}];
+	
+	flow();
+	run();
+	
+	return nil;
+}
+
+l3_test(@selector(red_reduce:usingBlock:)) {
+	REDConvolver *convolver = [[REDConvolver alloc] initWithReducibles:@[ @[ @1, @2, @3 ], @[ @4, @5, @6 ], @[ @7, @8, @9 ] ] convolution:^(id a, id b, id c){
+		return @[ a, b, c ];
+	}];
+	
+	NSArray *convoluted = [convolver red_reduce:nil usingBlock:^id(id into, id each) {
+		return each;
+	}];
+	l3_expect(convoluted).to.equal(@[ @3, @6, @9 ]);
 }
 
 @end
