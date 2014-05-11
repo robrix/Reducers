e3da782bf1a7ddf3daca014ed828d21116ac7d8d
Rob Rix <rob.rix@github.com>
2014-05-11 13:09:18 -0400

    Convolution takes iterables.

--- Reducers/REDConvolve.h
+++ Reducers/REDConvolve.h
@@ -6,15 +6,15 @@
 
 /// A block for the piecewise mapping of multiple collections (convolution).
 ///
-/// Although this is not declared with any parameters, it will actually receive one per reducible passed to \c REDConvolve.
+/// Although this is not declared with any parameters, it will actually receive one per iterable passed to \c REDConvolve.
 ///
 /// \return  The object to be produced at each step of the convolution.
 typedef id(^REDConvolutionBlock)();
 
 
-/// A reducer which applies a convolution to each element of each of a collection of reducibles.
+/// A reducer which applies a convolution to each element of each of a collection of iterables.
 ///
-/// \param reducibles   A reducible of the reducibles to convolve.
+/// \param iterables    A reducible of the iterables to convolve.
 /// \param convolution  A block to be applied to each element of the reducibles in turn.
 /// \return             A reducible which, when reduced, maps the elements of the reducibles produced by \c reducibles with \c convolution.
-id<REDReducible> REDConvolve(id<REDReducible> reducibles, REDConvolutionBlock convolution);
+id<REDReducible> REDConvolve(id<REDReducible> iterables, REDConvolutionBlock convolution);
