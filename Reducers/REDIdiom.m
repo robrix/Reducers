9b9ff3b4cb384be2c82c4600285ccb598b615518
Rob Rix <rob.rix@github.com>
2014-05-14 22:03:31 -0400

    Tests contrasting the originals and the idioms.

--- Reducers/REDIdiom.m
+++ Reducers/REDIdiom.m
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+#import "REDAppendable.h"
 #import "REDIdiom.h"
 #import "REDJoin.h"
 
@@ -20,13 +21,46 @@
 	return REDFilter(self, predicate);
 }
 
+l3_test(@selector(red_filter:)) {
+	NSArray *collection = @[ @"how", @"who", @"hear" ];
+	REDPredicateBlock startsWithH = ^bool (NSString *each) {
+		return [each hasPrefix:@"h"];
+	};
+	NSString *expected = @"howhear";
+	
+	// regular
+	l3_expect([@"" red_append:REDFilter(collection, startsWithH)]).to.equal(expected);
+	
+	// idiomatic (?)
+	l3_expect([@"" red_append:[collection red_filter:startsWithH]]).to.equal(expected);
+}
+
 
 -(id<REDReducible>)red_join:(id)separator {
 	return REDJoin(self, separator);
+}
+
+l3_test(@selector(red_join:)) {
+	l3_expect([@"" red_append:[@[ @"f", @"o", @"r", @"k" ] red_join:@" "]]).to.equal(@"f o r k");
 }
 
 
 -(id<REDIterable, REDReducible>)red_convolve:(REDConvolutionBlock)convolution {
 	return REDConvolve(self, convolution);
 }
+
+l3_test(@selector(red_convolve:)) {
+	NSArray *collections = @[ @"pork", @"rind" ];
+	REDConvolutionBlock convolution = ^(NSString *a, NSString *b) {
+		return [a stringByAppendingString:b];
+	};
+	NSString *interleaved = @"proirnkd";
+	
+	// regular
+	l3_expect([@"" red_append:REDConvolve(collections, convolution)]).to.equal(interleaved);
+	
+	// idiomatic (?)
+	l3_expect([@"" red_append:[collections red_convolve:convolution]]).to.equal(interleaved);
+}
+
 @end
