9e7fd840faef55ed402dba532513db506331e3a7
Rob Rix <rob.rix@github.com>
2014-05-17 00:32:51 -0400

    Stub in sources.

---
+++ Reducers/REDSource.h
@@ -0,0 +1,6 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import <Reducers/REDReducible.h>
+
+@interface REDSource : NSObject <REDReducible>
+@end
---
+++ Reducers/REDSource.m
@@ -0,0 +1,13 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "REDSource.h"
+
+@implementation REDSource
+
+#pragma mark REDReducible
+
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
+	return nil;
+}
+
+@end
