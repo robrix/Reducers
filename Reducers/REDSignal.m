f107454092a57761b7059389aaeb13d0595ec5ac
Rob Rix <rob.rix@github.com>
2014-05-17 00:32:07 -0400

    Stub in signals.

---
+++ Reducers/REDSignal.m
@@ -0,0 +1,13 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "REDSignal.h"
+
+@implementation REDSignal
+
+#pragma mark REDReducible
+
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
+	return nil;
+}
+
+@end
