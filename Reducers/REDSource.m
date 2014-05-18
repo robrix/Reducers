780dda048f8dc8f9b47a8cefb63a8dc12340a9fd
Rob Rix <rob.rix@github.com>
2014-05-17 21:06:09 -0400

    Maybe informal protocol.

--- Reducers/REDSource.m
+++ Reducers/REDSource.m
@@ -2,6 +2,13 @@
 
 #import "REDSource.h"
 
+@interface NSObject (REDObject)
+
+-(instancetype)red_object;
+
+@end
+
+
 @implementation REDSource
 
 #pragma mark REDIterable
@@ -22,6 +29,23 @@
 		*stop = into != [into self];
 	});
 	return [into self];
+}
+
+@end
+
+
+@implementation NSObject (REDObject)
+
+-(instancetype)red_object {
+	return self;
+}
+
+@end
+
+@implementation NSNull (REDObject)
+
+-(instancetype)red_object {
+	return nil;
 }
 
 @end
