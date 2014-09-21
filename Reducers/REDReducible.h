7278f0fe2a43cc07a2fab4c4a1e0c54ef655adc9
Rob Rix <rob.rix@github.com>
2014-09-21 17:13:48 -0400

    NSIndexSet is reducible.

--- Reducers/REDReducible.h
+++ Reducers/REDReducible.h
@@ -83,3 +83,8 @@ typedef id(^REDReducingBlock)(id into, id each);
 /// Reducing an enumerator with \c -red_reduce:usingBlock: uses \c -nextObject, and thus consumes all of the objects in the enumerator.
 @interface NSEnumerator (REDReducible) <REDReducible>
 @end
+
+
+/// \c NSIndexSet conforms to \c REDReducible.
+@interface NSIndexSet (REDReducible) <REDReducible>
+@end
